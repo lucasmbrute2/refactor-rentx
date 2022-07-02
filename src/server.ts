8296340676979befa33b683448b4d7c0bc584f67
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-02 03:58:27 -0300

    refactor: changed starter file name from "index" into "server"

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import { createConnection } from "./database/data-source";
 const app = express();
-app.use(express.json())
+app.use(express.json());
+createConnection();
 const PORT = 3333
+
+
 
 
 app.listen(PORT, () => {
