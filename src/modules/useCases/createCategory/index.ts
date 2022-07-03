96f2a56abfa6f04501e8e96a9eb75123be3ba4f9
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-03 17:02:31 -0300

    feat: all instances of creaty category

---
+++ src/modules/useCases/createCategory/index.ts
@@ -0,0 +1,9 @@
+import { CreateCategoryController } from "../createCategory/createCategoryController"
+import { CreateCategoryUseCase } from "../createCategory/createCategoryUseCase"
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository"
+
+const createCategoryRepository = new CategoriesRepository();
+const createCategoryUseCase = new CreateCategoryUseCase(createCategoryRepository);
+const createCategoryController = new CreateCategoryController(createCategoryUseCase);
+
+export { createCategoryController };
