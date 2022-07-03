6408870e28104ea86c0fad85bf87151c4b1a8a2b
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-03 16:50:06 -0300

    feat: implemented create categories useCase

---
+++ src/modules/useCases/createCategory/createCategoryUseCase.ts
@@ -0,0 +1,18 @@
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+export class CreateCategoryUseCase {
+    constructor(private categoriesRepository: ICategoriesRepository) { }
+
+    async execute({ name, description }: IRequest): Promise<void> {
+        const categoryAlreadyExists = await this.categoriesRepository.findByName(name);
+
+        if (categoryAlreadyExists) throw new Error("Category Already exists!")
+
+        await this.categoriesRepository.createCategory({ name, description })
+    }
+}
