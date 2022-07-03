8fdb1595fb66ffe5981d18cd46a9eb70a4cf358f
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-03 16:38:00 -0300

    feat: implemented interface from categories repository methods list and createCategory

--- src/modules/repositories/ICategoriesRepository.ts
+++ src/modules/repositories/ICategoriesRepository.ts
@@ -1,5 +1,13 @@
 import { Category } from "../entities/Category";
 
+export interface ICategoriesDTO {
+    name: string;
+    description: string;
+}
+
 export interface ICategoriesRepository {
-    findByName: (name: string) => Promise<Category>;
+    findByName(name: string): Promise<Category>
+    list(): Promise<Category[]>
+    createCategory({ name, description }: ICategoriesDTO): Promise<void>
+
 }
