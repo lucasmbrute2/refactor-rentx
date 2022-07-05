22f8b2ff0f7aa23181afc8cd12d314e492d28eb5
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-05 00:29:37 -0300

    feat: added a main to file handle all instances from list categories

---
+++ src/modules/useCases/listCategories/index.ts
@@ -0,0 +1,7 @@
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+import { ListCategoriesController } from "./listCategoriesController";
+import { ListCategoriesUseCase } from "./listCategoriesUseCase";
+
+const categoriesRepository = new CategoriesRepository()
+const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository)
+export const listCategoriesController = new ListCategoriesController(listCategoriesUseCase) 
