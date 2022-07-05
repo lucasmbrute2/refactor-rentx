5d2915b78f62e77d996d0dc61d91c8bfd839ff24
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-04 22:48:59 -0300

    fix: fixed router import

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
 import "reflect-metadata"
 import express from "express";
 import { AppDataSourceUseCases } from "./database/data-source";
-import { router } from "./routes"
+import { router } from "./routes/index"
 const app = express();
 app.use(express.json());
 app.use(router);
 
 AppDataSourceUseCases.createConnection();
 const PORT = 3333
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`)
