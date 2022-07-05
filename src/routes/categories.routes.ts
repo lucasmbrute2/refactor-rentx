a08ef36813119bffbc0bc6e1d6eb50af7905e112
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-05 00:29:50 -0300

    feat: added router to list all categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express"
 import { createCategoryController } from "../modules/useCases/createCategory/index"
+import { listCategoriesController } from "../modules/useCases/listCategories/index"
 
 const categoriesRouter = Router()
 
 categoriesRouter.post("/", (req, res) => {
     return createCategoryController.handle(req, res)
+})
+
+categoriesRouter.get("/", (req, res) => {
+    return listCategoriesController.handle(req, res)
 })
 
 export { categoriesRouter }
