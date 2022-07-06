f35b8de61add642d5f74c7f926b7d795ed80304f
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-06 01:09:25 -0300

    feat: Implemented Specification Repository

---
+++ src/modules/repositories/implementations/SpecificationReposity.ts
@@ -0,0 +1,23 @@
+import { Repository } from "typeorm";
+import { Specification } from "../../entities/Specification";
+import { AppDataSource } from "../../../database/data-source"
+import { ICreateSpecificationDTO, ISpecificationRepository } from "../ISpecificationReposity";
+
+export class SpecificationRepository implements ISpecificationRepository {
+    private repository: Repository<Specification>
+
+    constructor() {
+        this.repository = AppDataSource.getRepository(Specification)
+    }
+
+    async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
+        const specification = this.repository.create({ name, description })
+        await this.repository.save(specification)
+    }
+
+    async findByName(name: string): Promise<Specification | null> {
+        const specification = await this.repository.findOneBy({ name })
+        return specification;
+    }
+
+}
