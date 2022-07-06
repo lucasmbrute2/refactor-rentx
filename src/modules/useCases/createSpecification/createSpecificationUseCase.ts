c66f61fc07264b987c62f6675361c7a7504735e3
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-06 01:26:26 -0300

    fix: fixed Repository beeing build into Specification use case

--- src/modules/useCases/createSpecification/createSpecificationUseCase.ts
+++ src/modules/useCases/createSpecification/createSpecificationUseCase.ts
@@ -1,13 +1,12 @@
-import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
-import { ICreateSpecificationDTO } from "../../repositories/ISpecificationReposity";
+import { ICreateSpecificationDTO, ISpecificationRepository } from "../../repositories/ISpecificationReposity";
 
 export class createSpecificationUseCase {
-    constructor(private categoriesRepository: ICategoriesRepository) { }
+    constructor(private categoriesRepository: ISpecificationRepository) { }
 
     async execute({ name, description }: ICreateSpecificationDTO) {
         const specification = await this.categoriesRepository.findByName(name)
         if (specification) throw "Specification already exists"
 
-        await this.categoriesRepository.createCategory({ name, description })
+        await this.categoriesRepository.create({ name, description })
     }
 }
