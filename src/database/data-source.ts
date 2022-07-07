782a682b97f61e779652af39dea1cd26286fc0f7
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-07 00:45:47 -0300

    feat: implemented Specification migration

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -2,7 +2,7 @@ import { DataSource } from "typeorm";
 import { Category } from "../modules/entities/Category"
 import { Specification } from "../modules/entities/Specification";
 import { CreateCategories1654826967781 } from "./migrations/1656986536350-migrations"
-
+import { migrations1657165082853 } from "./migrations/1657165082853-migrations"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
@@ -15,7 +15,7 @@ export const AppDataSource = new DataSource({
     logging: true,
     entities: [Category, Specification],
     subscribers: [],
-    migrations: [CreateCategories1654826967781],
+    migrations: [CreateCategories1654826967781, migrations1657165082853],
 })
 export async function createConnection(): Promise<void> {
     try {
