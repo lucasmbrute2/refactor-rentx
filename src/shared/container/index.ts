dac81f284a9392edf9425c8ee7d37587ac4817a3
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-10 02:10:53 -0300

    feat: implemented tsyringe injection on specification

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,15 @@
 import { container } from "tsyringe"
 import { ICategoriesRepository } from "../../modules/repositories/ICategoriesRepository"
 import { CategoriesRepository } from "../../modules/repositories/implementations/CategoriesRepository"
+import { SpecificationRepository } from "../../modules/repositories/implementations/SpecificationRepository"
+import { ISpecificationRepository } from "../../modules/repositories/ISpecificationReposity"
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
     CategoriesRepository
+)
+
+container.registerSingleton<ISpecificationRepository>(
+    "SpecificationRepository",
+    SpecificationRepository
 )
