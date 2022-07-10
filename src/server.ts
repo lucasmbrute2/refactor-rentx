044079e2474db24e8018e255dd291543c602cd96
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-10 00:23:57 -0300

    feat: importing dependy injection file on main file

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import express from "express";
 import { createConnection } from "./database/data-source";
+import "./shared/container";
 import { router } from "./routes/index"
 const app = express();
 app.use(express.json());
