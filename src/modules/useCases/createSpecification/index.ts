969ef0cc7a8de5c2390aea161d333c1c4d7f7efc
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-10 02:08:31 -0300

    delete: removed file to manually inject dependency

--- src/modules/useCases/createSpecification/index.ts
+++
@@ -1,8 +0,0 @@
-import { SpecificationRepository } from "../../repositories/implementations/SpecificationRepository";
-import { CreateSpecificationController } from "./createSpecificationController";
-import { CreateSpecificationUseCase, } from "./createSpecificationUseCase";
-
-
-const specificationRepository = new SpecificationRepository()
-const createSpecificationUseCase = new CreateSpecificationUseCase(specificationRepository)
-export const createSpecificationController = new CreateSpecificationController(createSpecificationUseCase)
