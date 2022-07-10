8eae223992659503c529196e16e1ef2b3e119ef5
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-10 15:14:54 -0300

    remove: deleted file to manually instace dependencies

--- src/modules/useCases/listCategories/index.ts
+++
@@ -1,7 +0,0 @@
-import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
-import { ListCategoriesController } from "./listCategoriesController";
-import { ListCategoriesUseCase } from "./listCategoriesUseCase";
-
-const categoriesRepository = new CategoriesRepository()
-const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository)
-export const listCategoriesController = new ListCategoriesController(listCategoriesUseCase) 
