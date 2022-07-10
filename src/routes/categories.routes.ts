542e5f8c6b8a4cb559ef4034345d5e608b5027d3
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-10 15:10:59 -0300

    feat : implemented multer to import files

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,8 +2,12 @@ import { Router } from "express"
 import { CreateCategoryController } from "../modules/useCases/createCategory/createCategoryController"
 import { listCategoriesController } from "../modules/useCases/listCategories/index"
 import { ImportCategoryController } from "../modules/useCases/importCategory/importCategoryController"
+import multer from "multer"
 
 const categoriesRouter = Router()
+const upload = multer({
+    dest: "./temp"
+})
 
 const createCategoryController = new CreateCategoryController()
 const importCategoryController = new ImportCategoryController()
@@ -13,6 +17,6 @@ categoriesRouter.get("/", (req, res) => {
     return listCategoriesController.handle(req, res)
 })
 
-categoriesRouter.post("/import", importCategoryController.handle)
+categoriesRouter.post("/import", upload.single("file"), importCategoryController.handle)
 
 export { categoriesRouter }
