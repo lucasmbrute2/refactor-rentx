6bbae5dc8174f4ba8954764886c6ac3405f454ef
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-10 00:20:46 -0300

    delete: removing past instances file

--- src/modules/useCases/createCategory/index.ts
+++
@@ -1,9 +0,0 @@
-import { CreateCategoryController } from "../createCategory/createCategoryController"
-import { CreateCategoryUseCase } from "../createCategory/createCategoryUseCase"
-import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository"
-
-
-const createCategoryRepository = new CategoriesRepository();
-const createCategoryUseCase = new CreateCategoryUseCase(createCategoryRepository);
-const createCategoryController = new CreateCategoryController(createCategoryUseCase);
-export { createCategoryController }
