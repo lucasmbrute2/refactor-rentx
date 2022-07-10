1d08f5934e83a283260bee468d3fda00082a5418
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-10 15:15:31 -0300

    refactor: dependency injenction with tsyringe on controller

--- src/modules/useCases/listCategories/listCategoriesController.ts
+++ src/modules/useCases/listCategories/listCategoriesController.ts
@@ -1,12 +1,14 @@
 import { ListCategoriesUseCase } from "./listCategoriesUseCase";
 import { Request, Response } from "express"
+import { container } from "tsyringe";
 
 export class ListCategoriesController {
-    constructor(private listCategoriesUseCase: ListCategoriesUseCase) { }
-
     async handle(req: Request, res: Response): Promise<Response> {
+
+        const listCategoriesUseCase = container.resolve(ListCategoriesUseCase)
+
         try {
-            const allCategories = await this.listCategoriesUseCase.execute()
+            const allCategories = await listCategoriesUseCase.execute()
             return res.json(allCategories)
         } catch (error) {
             return res.status(404).send({ message: "Categories not found" })
