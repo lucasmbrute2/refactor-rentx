591fa946722828f91cece6f548d302c0bf1c9ee6
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-10 02:37:12 -0300

    feat: implemented import file route

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,15 +1,16 @@
 import { Router } from "express"
 import { CreateCategoryController } from "../modules/useCases/createCategory/createCategoryController"
 import { listCategoriesController } from "../modules/useCases/listCategories/index"
+import { ImportCategoryController } from "../modules/useCases/importCategory/importCategoryController"
 
 const categoriesRouter = Router()
 
 const createCategoryController = new CreateCategoryController()
-
+const importCategoryController = new ImportCategoryController()
 categoriesRouter.post("/", createCategoryController.handle)
 
-categoriesRouter.get("/", (req, res) => {
-    return listCategoriesController.handle(req, res)
-})
+categoriesRouter.get("/",)
+
+categoriesRouter.post("/import", importCategoryController.handle)
 
 export { categoriesRouter }
