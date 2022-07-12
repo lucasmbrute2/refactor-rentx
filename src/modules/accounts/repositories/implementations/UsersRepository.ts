16cb4e3215cf14ace77f4250646f735326899d0e
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-12 03:35:54 -0300

    feat: implemented user repository

---
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,25 @@
+import { ICreateUsersDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+import { AppDataSource } from "../../../../database/data-source"
+import { Repository } from "typeorm";
+
+export class UsersRepository implements IUsersRepository {
+    private repository: Repository<User>;
+    constructor() {
+        this.repository = AppDataSource.getRepository(User)
+    }
+
+    async create(data: ICreateUsersDTO): Promise<void> {
+        const user = this.repository.create(data);
+
+        await this.repository.save(user);
+
+    }
+
+    async list(): Promise<User[]> {
+        const users = await this.repository.find();
+        return users
+    }
+
+}
