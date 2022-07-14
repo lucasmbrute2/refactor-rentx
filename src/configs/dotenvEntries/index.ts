3e87b36585a7ef93ab865659926a120a4cd033f2
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-14 03:25:29 -0300

    feat: implemented dotenv

---
+++ src/configs/dotenvEntries/index.ts
@@ -0,0 +1,10 @@
+export default {
+    database: {
+        databaseName: process.env.DB_NAME ?? "postgres",
+        databasePassword: process.env.DB_PASSWORD ?? "postgres",
+        databaseUsername: process.env.DB_DB_USERNAME ?? "postgres"
+    },
+    token: {
+        md5Hash: process.env.MD5HASH_JSON_TOKEN ?? ''
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import express from "express";
 import { createConnection } from "./database/data-source";
 import "./shared/container";
 import { router } from "./routes/index"
+import dotenv from "dotenv"
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(router);
