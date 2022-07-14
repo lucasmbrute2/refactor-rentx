260f7cca78cb62f3605a3c0ada390004a9d758a0
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-14 02:06:31 -0300

    feat: added migration to delete "username" column

--- src/database/migrations/index.ts
+++ src/database/migrations/index.ts
@@ -1,9 +1,11 @@
 import { migrations1657165082853 } from "../migrations/1657165082853-migrations"
 import { CreateCategories1654826967781 } from "../migrations/1656986536350-migrations"
 import { User1657602031073 } from "../migrations/1657602031073-User"
+import { AlterUserDeleteUserName1657774263727 } from "../migrations/1657774263727-AlterUserDeleteUserName"
 
 export const migrations = [
     CreateCategories1654826967781,
     migrations1657165082853,
-    User1657602031073
+    User1657602031073,
+    AlterUserDeleteUserName1657774263727
 ]
