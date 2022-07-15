3377c14fee2f9ed20f83b13213ddf8e0d44da5c1
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-15 03:11:03 -0300

    feat: middleware to handle errors on server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,25 @@
 import "reflect-metadata";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { createConnection } from "./database/data-source";
 import "./shared/container";
 import { router } from "./routes/index"
+import { AppError } from "./errors/AppError";
 
 const app = express();
 app.use(express.json());
 app.use(router);
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+        return res.status(err.statusCode).json({
+            message: err.message
+        })
+    }
+
+    return res.status(500).json({
+        status: "error",
+        message: `Internal server error - ${err.message}`
+    })
+})
 
 createConnection();
 const PORT = 3333
