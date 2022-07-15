e820ae2c2c37c0e1ae43ca1ed23305781fb6a101
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-15 01:47:01 -0300

    feat: using auth middleware

--- src/routes/specification.routes.ts
+++ src/routes/specification.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express"
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/createSpecificationController"
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const specificationRouter = Router()
 
 const createSpecificationController = new CreateSpecificationController();
 
+specificationRouter.use(ensureAuthenticated)
 specificationRouter.post("/", createSpecificationController.handle)
 
 export { specificationRouter }
