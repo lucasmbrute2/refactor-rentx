bd54ffb87de1b9f9137654d73aeb2129f28fe81b
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-20 02:45:58 -0300

    fix: removing unecessary try catch

--- src/modules/cars/useCases/createCategory/createCategoryController.ts
+++ src/modules/cars/useCases/createCategory/createCategoryController.ts
@@ -1,19 +1,15 @@
 import { Request, Response } from "express"
 import { CreateCategoryUseCase } from "../createCategory/createCategoryUseCase"
 import { container } from "tsyringe"
+import { AppError } from "../../../../errors/AppError";
 
 export class CreateCategoryController {
     async handle(req: Request, res: Response): Promise<Response> {
         const { name, description } = req.body;
 
         const createCategoryUseCase = container.resolve(CreateCategoryUseCase)
+        await createCategoryUseCase.execute({ name, description });
 
-        try {
-            await createCategoryUseCase.execute({ name, description });
-        } catch (error) {
-            res.status(400).send(error);
-        }
         return res.status(201).send();
     }
-
 }
