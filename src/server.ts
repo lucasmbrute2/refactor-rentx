c85b9979a3ba2f09840d3c24011d6aeda0d97add
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-20 02:23:45 -0300

    feat: implemented express-async-errors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors"
 import { createConnection } from "./database/data-source";
 import "./shared/container";
 import { router } from "./routes/index"
