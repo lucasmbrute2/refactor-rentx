f80bb282118545e99a0f98dd12db9c8732b3fce8
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-22 00:21:09 -0300

    feat: added ensure auth to update avatar route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -4,6 +4,7 @@ const userRouter = Router();
 import uploadConfig from "../configs/uploads/upload"
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController"
 import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated"
 
 const createUserController = new CreateUserController();
 const updateAvatarUserController = new UpdateUserAvatarController();
@@ -11,6 +12,6 @@ const updateAvatarUserController = new UpdateUserAvatarController();
 const uploadAvatar = multer(uploadConfig.upload("./temp/avatar"))
 
 userRouter.post("/", createUserController.handle)
-userRouter.patch("/avatar", uploadAvatar.single("avatar"), updateAvatarUserController.handle)
+userRouter.patch("/avatar", ensureAuthenticated, uploadAvatar.single("avatar"), updateAvatarUserController.handle)
 
 export { userRouter }
