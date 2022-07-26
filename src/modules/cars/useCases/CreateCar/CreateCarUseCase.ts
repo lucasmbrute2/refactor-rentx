3e8711650ea130bdb3894a2d27769b8a2ac34e38
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-26 03:13:31 -0300

    feat: implemented CreateCar use case

---
+++ src/modules/cars/useCases/CreateCar/CreateCarUseCase.ts
@@ -0,0 +1,25 @@
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
+import { ICarsRepository, ICreateCarDTO } from "@modules/cars/repositories/ICarsRepository";
+import { AppError } from "@shared/errors/AppError";
+import { inject, injectable } from "tsyringe";
+
+
+
+// @injectable()
+export class CreateCarUseCase {
+
+    constructor(
+        // @inject("CarsRepository")
+        private carsRepository: ICarsRepository
+    ) { }
+
+
+    async execute(data: ICreateCarDTO): Promise<Car> {
+
+        const carAlreadyExists = await this.carsRepository.findByLicensePlate(data.license_plate)
+
+        if (carAlreadyExists) throw new AppError("Car Already exists!");
+
+        return await this.carsRepository.create(data)
+    }
+}
