b0624844abcf940571754d19e63df26f90bb5bdf
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-26 00:16:23 -0300

    feat: added "cars" table migration

--- src/shared/typeorm/migrations/index.ts
+++ src/shared/typeorm/migrations/index.ts
@@ -3,11 +3,13 @@ import { CreateCategories1654826967781 } from "./1656986536350-migrations"
 import { User1657602031073 } from "./1657602031073-User"
 import { AlterUserDeleteUserName1657774263727 } from "./1657774263727-AlterUserDeleteUserName"
 import { AlterUserAddAvatar1658376169074 } from "./1658376169074-AlterUserAddAvatar"
+import { CreateCars1658791215491 } from "./1658791215491-CreateCars"
 
 export const migrations = [
     CreateCategories1654826967781,
     migrations1657165082853,
     User1657602031073,
     AlterUserDeleteUserName1657774263727,
-    AlterUserAddAvatar1658376169074
+    AlterUserAddAvatar1658376169074,
+    CreateCars1658791215491
 ]
