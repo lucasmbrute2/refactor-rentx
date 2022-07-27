0f856eb90ebf0056af1d4a5a15e584c17d1ae88d
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-27 20:34:08 -0300

    inserted into a typeorm folder

--- src/shared/infra/typeorm/data-source.ts
+++ src/shared/infra/typeorm/data-source.ts
@@ -1,7 +1,7 @@
 import { DataSource } from "typeorm";
 
 import { migrations } from "./migrations"
-import dontenv from "../../configs/dotenvEntries"
+import dontenv from "../../../configs/dotenvEntries"
 import { User } from "@modules/accounts/infra/typeorm/entities/User";
 import { Category } from "@modules/cars/infra/typeorm/entities/Category";
 import { Specification } from "@modules/cars/infra/typeorm/entities/Specification";
