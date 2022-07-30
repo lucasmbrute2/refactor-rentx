0954316b5fbeef4003593d6fe2f4a1ceb17de382
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-30 17:39:36 -0300

    feat: Implemented Cars Specification Many to Many migration

---
+++ src/shared/infra/typeorm/migrations/1659206068902-CreateSpecificationCars.ts
@@ -0,0 +1,59 @@
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from "typeorm"
+
+export class CreateSpecificationCars1659206068902 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "specifications_cars",
+            columns: [
+                {
+                    name: "car_id",
+                    type: "uuid"
+                },
+                {
+                    name: "specification_id",
+                    type: "uuid"
+                },
+                {
+                    name: "created_at",
+                    type: "timestamp",
+                    default: "now()"
+                }
+            ]
+        }))
+
+        await queryRunner.createForeignKey(
+            "specifications_cars",
+            new TableForeignKey({
+                name: "FKSpecificationCar",
+                referencedTableName: "specifications",
+                referencedColumnNames: ["id"],
+                columnNames: ["specification_id"],
+                onDelete: "SET NULL",
+                onUpdate: "SET NULL"
+            })
+        )
+
+        await queryRunner.createForeignKey(
+            "specifications_cars",
+            new TableForeignKey({
+                name: "FKCarsSpecification",
+                columnNames: ["car_id"],
+                referencedColumnNames: ["id"],
+                referencedTableName: "cars",
+                onDelete: "SET NULL",
+                onUpdate: "SET NULL"
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        const table = await queryRunner.getTable("specifications_cars")
+        const foreignKeys = table?.foreignKeys.filter(fk => (fk.columnNames.indexOf("car_id") || fk.columnNames.indexOf("specification_id") !== -1))
+
+        if (foreignKeys) await queryRunner.dropForeignKeys("specifications_cars", foreignKeys)
+
+        await queryRunner.dropTable("specifications_cars")
+    }
+
+}
--- src/shared/infra/typeorm/migrations/index.ts
+++ src/shared/infra/typeorm/migrations/index.ts
@@ -4,6 +4,7 @@ import { User1657602031073 } from "./1657602031073-User"
 import { AlterUserDeleteUserName1657774263727 } from "./1657774263727-AlterUserDeleteUserName"
 import { AlterUserAddAvatar1658376169074 } from "./1658376169074-AlterUserAddAvatar"
 import { CreateCars1658791215491 } from "./1658791215491-CreateCars"
+import { CreateSpecificationCars1659206068902 } from "./1659206068902-CreateSpecificationCars"
 
 export const migrations = [
     CreateCategories1654826967781,
@@ -11,5 +12,6 @@ export const migrations = [
     User1657602031073,
     AlterUserDeleteUserName1657774263727,
     AlterUserAddAvatar1658376169074,
-    CreateCars1658791215491
+    CreateCars1658791215491,
+    CreateSpecificationCars1659206068902
 ]
