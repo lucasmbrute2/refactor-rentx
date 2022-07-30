ae3d9d97c5f1ad6cc976e80eae5f5f697d1e4b72
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-29 23:20:29 -0300

    feat: added middlewares to validate login and if is a admin

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,9 +1,11 @@
 import { CreateCarController } from "@modules/cars/useCases/CreateCar/CreateCarController";
 import { Router } from "express"
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 const carsRouter = Router();
 
 const createCarController = new CreateCarController()
 
-carsRouter.post("/", createCarController.handle)
+carsRouter.post("/", ensureAuthenticated, ensureAdmin, createCarController.handle)
 
 export { carsRouter }
