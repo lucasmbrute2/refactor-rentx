3769d698a40b14e6657a633db542b720d5ecba14
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-30 17:38:33 -0300

    chore: Change entity name from "cars" to "Car"

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -2,7 +2,7 @@ import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn 
 import { v4 as uuid } from "uuid"
 import { Category } from "./Category";
 
-@Entity("cars")
+@Entity("Car")
 export class Car {
     @PrimaryColumn()
     id?: string;
