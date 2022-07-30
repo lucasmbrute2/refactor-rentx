bbe6f0d1fc4c7d37d6d71589f90de332e532b312
Lucas Dantas <lucasmbrute614@gmail.com>
2022-07-30 04:05:48 -0300

    feat: implemented list available cars route

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,11 +1,14 @@
 import { CreateCarController } from "@modules/cars/useCases/CreateCar/CreateCarController";
+import { ListCarsController } from "@modules/cars/useCases/ListCars/ListCarsController";
 import { Router } from "express"
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 const carsRouter = Router();
 
 const createCarController = new CreateCarController()
+const listAvailableControler = new ListCarsController()
 
 carsRouter.post("/", ensureAuthenticated, ensureAdmin, createCarController.handle)
+carsRouter.get("/available", listAvailableControler.handle)
 
 export { carsRouter }
