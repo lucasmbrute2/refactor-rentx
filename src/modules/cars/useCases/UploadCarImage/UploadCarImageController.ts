5ef9eb987dda55d2040b9e07d8149dd641cfbf2c
Lucas Dantas <lucasmbrute614@gmail.com>
2022-08-01 22:41:29 -0300

    feat: implemented UploadCarImage controller

---
+++ src/modules/cars/useCases/UploadCarImage/UploadCarImageController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { UploadCarImageUseCase } from "./UploadCarImageUseCase";
+
+interface IFiles {
+    filename: string;
+}
+
+export class UploadCarController {
+    async handle(req: Request, res: Response): Promise<Response> {
+        const { id } = req.params
+        const images = req.files as IFiles[]
+
+        const uploadCarImagUseCase = container.resolve(UploadCarImageUseCase)
+
+        const images_name = images?.map(file => file.filename)
+
+        await uploadCarImagUseCase.execute({
+            car_id: id,
+            images_name
+        })
+
+        return res.status(201).send();
+    }
+}
