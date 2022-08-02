271c9ad3b731b7884dd5e58688a9afec46263eb4
Lucas Dantas <lucasmbrute614@gmail.com>
2022-08-01 22:41:15 -0300

    feat: implemented CarsImages use case

---
+++ src/modules/cars/useCases/UploadCarImage/UploadCarImageUseCase.ts
@@ -0,0 +1,21 @@
+import { ICarsImagesRepositories } from "@modules/cars/repositories/ICarsImagesRepositories";
+import { inject, injectable } from "tsyringe";
+
+interface IRequest {
+    car_id: string;
+    images_name: string[];
+}
+
+@injectable()
+export class UploadCarImageUseCase {
+    constructor(
+        @inject("CarsImagesRepository")
+        private carsImages: ICarsImagesRepositories
+    ) { }
+
+    async execute({ car_id, images_name }: IRequest) {
+        images_name.forEach(async (image) => {
+            await this.carsImages.create(car_id, image)
+        })
+    }
+}
