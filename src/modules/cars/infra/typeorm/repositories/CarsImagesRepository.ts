54fddfdf8048e071494fb814deb755fcdd4d81df
Lucas Dantas <lucasmbrute614@gmail.com>
2022-08-01 22:23:28 -0300

    feat: implemented CarsImage repository with create method

---
+++ src/modules/cars/infra/typeorm/repositories/CarsImagesRepository.ts
@@ -0,0 +1,22 @@
+import { ICarsImagesRepositories } from "@modules/cars/repositories/ICarsImagesRepositories";
+import { AppDataSource } from "@shared/infra/typeorm/data-source";
+import { Repository } from "typeorm";
+import { CarImage } from "../entities/CarImages";
+
+export class CarsImagesRepository implements ICarsImagesRepositories {
+    private repository: Repository<CarImage>
+
+    constructor() {
+        this.repository = AppDataSource.getRepository(CarImage)
+    }
+
+    async create(car_id: string, image_name: string): Promise<CarImage> {
+        const carImage = this.repository.create({
+            car_id,
+            image_name
+        })
+
+        return await this.repository.save(carImage)
+    }
+
+}
