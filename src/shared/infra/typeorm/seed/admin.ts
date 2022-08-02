c8c832cfb16225ac7596fdf3e3ac0f8ffaf3f863
Lucas Dantas <lucasmbrute614@gmail.com>
2022-08-01 22:21:49 -0300

    fix: Fixed id to admin creation

--- src/shared/infra/typeorm/seed/admin.ts
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -1,15 +1,15 @@
 import { AppDataSource, createConnection } from "../data-source";
 import { hash } from "bcrypt"
 import config from "@configs/dotenvEntries/index"
+import { v4 as uuid } from "uuid"
 
 async function create() {
     await createConnection()
-    const id = Math.floor(Math.random() * 300)
     const password = await hash(config.password, 8)
 
     await AppDataSource.query(`
     INSERT INTO USERS( id, name, email, password, "isAdmin", created_at, driver_license )
-    values('${id}','admin', 'admin@rentx.com', '${password}', true, 'now()', 'XXXXXXXX' )
+    values('${uuid()}','admin', 'admin@rentx.com', '${password}', true, 'now()', 'XXXXXXXX' )
     `)
 
     await AppDataSource.destroy()
