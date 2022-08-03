3aaa2f240ce338223c1df20ab890faca49af5d62
Lucas Dantas <lucasmbrute614@gmail.com>
2022-08-03 00:40:45 -0300

    feat: implemented Interface to Rental Repository

---
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -0,0 +1,8 @@
+import { Rental } from "../infra/typeorm/entities/Rental";
+import { IRequest } from "../useCases/createRental/CreateRentalUseCase";
+
+export interface IRentalsRepository {
+    create({ car_id, user_id, expected_return_date }: IRequest): Promise<Rental>
+    findByCarID(car_id: string): Promise<Rental | Falsy>
+    findByUser(user_id: string): Promise<Rental | Falsy>
+}
