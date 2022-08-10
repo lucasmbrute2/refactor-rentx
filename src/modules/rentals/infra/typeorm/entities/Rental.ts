98047f27dd88f58aebc88b25cce14c8c779fc62a
Lucas Dantas <lucasmbrute614@gmail.com>
2022-08-10 02:29:53 -0300

    feat: added typeorm to Rental entity

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -1,25 +1,34 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 import { v4 as uuid } from "uuid"
 
+@Entity()
 export class Rental {
+    @PrimaryGeneratedColumn()
     id?: string;
 
+    @Column()
     car_id!: string;
 
+    @Column()
     user_id!: string;
 
+    @Column()
     start_date!: Date;
 
+    @Column()
     end_date!: Date;
 
+    @Column()
     expected_return_data!: Date;
 
+    @Column()
     total!: number;
 
+    @Column()
     created_at!: Date;
 
+    @Column()
     updated_at!: Date;
-
 
     constructor() {
         if (!this.id) this.id = uuid()
