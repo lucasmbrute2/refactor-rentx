933320f3770bb264cf8c9757fb36d33d2e1937f3
Lucas Dantas <lucasmbrute614@gmail.com>
2022-08-11 21:40:48 -0300

    feat: implemented expected return date verification

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,6 +1,9 @@
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository"
 import { AppError } from "@shared/errors/AppError";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc"
+dayjs.extend(utc)
 
 export interface IRequest {
     user_id: string;
@@ -12,13 +15,21 @@ export class CreateRentalUseCase {
     constructor(
         private rentalsRepository: IRentalsRepository
     ) { }
-
     async execute({ car_id, user_id, expected_return_date }: IRequest): Promise<Rental> {
+
+        const MINIMUM_COMPARE_HOURS = 24;
         const carAlreadyRented = await this.rentalsRepository.findByCarID(car_id)
         const userAlreadyRentedACar = await this.rentalsRepository.findByUser(user_id)
 
         if (carAlreadyRented) throw new AppError("This car is already rented");
         if (userAlreadyRentedACar) throw new AppError("User has already rented a car");
+
+        const expectedReturnDateFormat = dayjs(expected_return_date).utc().local().format()
+        const dateNow = dayjs().utc().local().format()
+
+        const compare = dayjs(expectedReturnDateFormat).diff(dateNow, "hours")
+
+        if (compare < MINIMUM_COMPARE_HOURS) throw new AppError("Invalid return time.")
 
         const car = this.rentalsRepository.create({
             car_id,
