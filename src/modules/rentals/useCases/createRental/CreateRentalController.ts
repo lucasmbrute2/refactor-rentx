3417c1aea26c3383fc38b3e25c721763561cbfee
Lucas Dantas <lucasmbrute614@gmail.com>
2022-08-15 22:45:50 -0300

    feat: implemented CreateRental Controller

--- src/modules/rentals/useCases/createRental/CreateRentalController.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalController.ts
@@ -1,3 +1,19 @@
-export class CreateRentalController {
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { CreateRentalUseCase } from "./CreateRentalUseCase";
 
+export class CreateRentalController {
+    async handle(req: Request, res: Response): Promise<Response> {
+        const createRentalUseCase = container.resolve(CreateRentalUseCase)
+        const { car_id, expected_return_date } = req.body
+        const { id: user_id } = req.user
+
+        const rental = await createRentalUseCase.execute({
+            car_id,
+            user_id,
+            expected_return_date
+        })
+
+        return res.status(201).json(rental)
+    }
 }
