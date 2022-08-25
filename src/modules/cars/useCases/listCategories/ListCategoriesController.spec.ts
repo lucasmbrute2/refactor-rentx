b1bcc4ed5643a5064d6ccf0d653dfc9fe8973322
Lucas Dantas <lucasmbrute614@gmail.com>
2022-08-25 02:59:37 -0300

    feat: implemented List Categories specs

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -0,0 +1,29 @@
+import { clearDB, create } from "@shared/infra/typeorm/seed/admin"
+import request from "supertest"
+import config from "@configs/dotenvEntries"
+import { app } from "@shared/infra/http/app"
+
+describe("List Category", () => {
+    beforeEach(async () => {
+        await clearDB()
+        await create()
+    })
+
+    it("should be able to list all categories", async () => {
+        const { body: { token } } = await request(app).post("/sessions").send({
+            email: "admin@rentx.com",
+            password: config.password
+        })
+
+        await request(app).post("/categories").send({
+            name: "sedan TEST4",
+            description: "Carro alogando e baixo TEST4"
+        }).set({
+            Authorization: `Bearer ${token}`
+        })
+
+        const response = await request(app).get("/categories")
+
+        expect(response.status).toBe(200)
+    })
+})
