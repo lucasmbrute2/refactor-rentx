f6b10e3fa520b7c25b26c51ef395d48b263cb335
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-08-31 02:06:54 -0300

    feat: implemented List rentals conttroller

---
+++ src/modules/rentals/useCases/listrentalsByUser/ListrentalsByUserController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { ListrentalsByUserUseCase } from "./listrentalsByUserUseCase";
+
+export class ListrentalsByUserController {
+    async handle(req: Request, res: Response) {
+        const listrentalsByUserUseCase = container.resolve(ListrentalsByUserUseCase)
+        const { id: user_id } = req.user
+
+        const rentals = await listrentalsByUserUseCase.execute(+user_id)
+
+        return res.json(rentals).send()
+    }
+}
