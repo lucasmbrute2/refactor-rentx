91adfa5bf53f974f8563f6dbec1b0866a0104d60
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-08-31 17:15:09 -0300

    feat: users_tokens migration

---
+++ src/shared/infra/typeorm/migrations/1661976692470-CreateUserToken.ts
@@ -0,0 +1,47 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
+
+export class CreateUserToken1661976692470 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "users_tokens",
+                columns: [
+                    {
+                        name: "id",
+                        type: "integer",
+
+                    },
+                    {
+                        name: "refresh_token",
+                        type: "varchar"
+                    },
+                    {
+                        name: "user_id",
+                        type: "integer"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ],
+                foreignKeys: [
+                    {
+                        name: "FKUserToken",
+                        referencedTableName: "users",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["user_id"],
+                        onDelete: "CASCADE",
+                        onUpdate: "CASCADE"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users_tokens")
+    }
+
+}
