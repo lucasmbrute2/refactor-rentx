d6dd1055f9cfbfdbfc61d7a30191f25585be2c29
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-08-31 02:07:00 -0300

    feat: implemented List rentals route

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,13 +1,16 @@
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
 import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
+import { ListrentalsByUserController } from "@modules/rentals/useCases/listrentalsByUser/ListrentalsByUserController";
 import { Router } from "express"
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 const rentalRouter = Router();
 
 const createRentalController = new CreateRentalController()
 const devolutionRentalController = new DevolutionRentalController()
+const listrentalsByUserController = new ListrentalsByUserController()
 
 rentalRouter.post("/", ensureAuthenticated, createRentalController.handle)
 rentalRouter.put("/devolution/:id", ensureAuthenticated, devolutionRentalController.handle)
+rentalRouter.get("/user", ensureAuthenticated, listrentalsByUserController.handle)
 
 export { rentalRouter }
