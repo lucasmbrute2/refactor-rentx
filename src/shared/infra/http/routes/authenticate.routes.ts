fbc8b0540839217040e8d1a6ad4499c78a1561fb
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-09-04 21:37:55 -0300

    feat: implemented Refresh token route

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 const authenticateRouter = Router();
 import { AuthenticateUserController } from "@modules/accounts/useCases/authenticateUser/AuthenticateUserController"
+import { RefreshTokenController } from "@modules/accounts/useCases/refreshToken/RefreshTokenController";
 
 const authenticateUserController = new AuthenticateUserController();
+const refreshTokenController = new RefreshTokenController();
 
 authenticateRouter.post("/sessions", authenticateUserController.handle)
+authenticateRouter.post("/refresh-token", refreshTokenController.handle)
 
 export { authenticateRouter }
