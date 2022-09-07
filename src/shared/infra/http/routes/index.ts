5e0a8ac86ae9eaaa0fa4e967f94c4fe7c2ce36ab
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-09-07 20:05:41 -0300

    feat: Implemented forgot password routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { authenticateRouter } from "./authenticate.routes";
 import { carsRouter } from "./cars.routes";
 import { categoriesRouter } from "./categories.routes"
+import { passwordRoutes } from "./password.routes";
 import { rentalRouter } from "./rental.routes";
 import { specificationRouter } from "./specification.routes";
 import { userRouter } from "./user.routes";
@@ -14,5 +15,6 @@ router.use("/user", userRouter)
 router.use("/car", carsRouter)
 router.use("/rentals", rentalRouter)
 router.use(authenticateRouter)
+router.use("/password", passwordRoutes)
 
 export { router }
