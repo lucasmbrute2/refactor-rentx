01994a1e729ea6adff102e36e70d31984c3c1a5c
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-09-10 12:55:29 -0300

    feat: implemented specs to send email forgot password

---
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.spec.ts
@@ -0,0 +1,38 @@
+import { UsersRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersRepositoryInMemory"
+import { UsersTokensRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory"
+import { DayJsDateProvider } from "@shared/container/providers/DateProvider/DayJsDateProvider"
+import { MailProviderInMemory } from "@shared/container/providers/MailProvider/in-memory/MailProviderInMemory"
+import { AppError } from "@shared/errors/AppError"
+import { SendForgotPasswordMailUseCase } from "./SendForgotPasswordMailUseCase"
+
+let sendForgotPasswordMailUseCase: SendForgotPasswordMailUseCase
+let usersRepositoryInMemory: UsersRepositoryInMemory
+let dateProvider: DayJsDateProvider
+let usersTokensRepositoryInMemory: UsersTokensRepositoryInMemory
+let mailProvider: MailProviderInMemory;
+
+describe("Send forgot mail", () => {
+    beforeEach(() => {
+        usersRepositoryInMemory = new UsersRepositoryInMemory();
+        usersTokensRepositoryInMemory = new UsersTokensRepositoryInMemory();
+        dateProvider = new DayJsDateProvider();
+        mailProvider = new MailProviderInMemory();
+        sendForgotPasswordMailUseCase = new SendForgotPasswordMailUseCase(usersRepositoryInMemory, usersTokensRepositoryInMemory, dateProvider, mailProvider);
+    })
+
+    it("should be able to send a forgot email to user", async () => {
+        const sendMail = jest.spyOn(mailProvider, "sendMail")
+
+        await usersRepositoryInMemory.create({
+            driver_license: "driver license TEST",
+            email: "email TEST",
+            name: "name TEST",
+            password: "123467",
+            avatar: null
+        })
+
+        await sendForgotPasswordMailUseCase.execute("email TEST");
+
+        expect(sendMail).toHaveBeenCalled();
+    })
+})
