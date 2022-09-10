8af39aba53a21c2d8c6895c0c9ccbd913ff1ab90
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-09-10 12:54:54 -0300

    chore: adding coverage reprots to jest

--- jest.config.ts
+++ jest.config.ts
@@ -7,6 +7,15 @@ export default {
     coverageProvider: "v8",
     moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/src" }),
     preset: "ts-jest",
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "<rootDir>/src/modules/**/useCases/**/*.ts"
+    ],
+    coverageDirectory: "coverage",
+    coverageReporters: [
+        "text-summary",
+        "lcov"
+    ],
     testMatch: [
         "**/*.spec.ts"
     ],
