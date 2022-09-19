6618fa88aeeaa986abea6c526222757f71ebede9
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-09-19 00:08:52 -0300

    refac: update now use Storage provider

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,7 +1,7 @@
 import { AppError } from "@shared/errors/AppError";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
-import { deleteFile } from "@utils/file";
 import { inject, injectable } from "tsyringe";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
 
 interface IRequest {
     user_id: number;
@@ -12,13 +12,17 @@ interface IRequest {
 export class UpdateUserAvatarUseCase {
     constructor(
         @inject("UsersRepository")
-        private userRepository: IUsersRepository
+        private userRepository: IUsersRepository,
+        @inject("StorageProvider")
+        private storageProvider: IStorageProvider
     ) { }
 
     async execute({ user_id, avatar_file }: IRequest) {
         const user = await this.userRepository.findByID(user_id)
 
-        if (user?.avatar) await deleteFile(`./temp/avatar/${user.avatar}`)
+
+        if (user?.avatar) await this.storageProvider.delete(user.avatar, "avatar")
+        await this.storageProvider.save(avatar_file, "avatar")
 
         if (!user) throw new AppError("User not found.");
 
