5722356649aa354bb7e8cb20a36f8b974e43c466
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-09-19 00:08:10 -0300

    feat: implemented IStorageProvider container

---
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -0,0 +1,8 @@
+import { container } from "tsyringe";
+import { LocalStorageProvider } from "./implementations/LocalStorageProvider";
+import { IStorageProvider } from "./IStorageProvider";
+
+container.registerSingleton<IStorageProvider>(
+    "StorageProvider",
+    LocalStorageProvider
+)
