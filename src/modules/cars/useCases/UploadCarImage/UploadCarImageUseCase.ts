fcb4c96d874dd4620835d742d8906f34cc2b6a80
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-09-20 13:08:50 -0300

    feat: Adding S3 method to car image

--- src/modules/cars/useCases/UploadCarImage/UploadCarImageUseCase.ts
+++ src/modules/cars/useCases/UploadCarImage/UploadCarImageUseCase.ts
@@ -1,4 +1,5 @@
 import { ICarsImagesRepositories } from "@modules/cars/repositories/ICarsImagesRepositories";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
 import { inject, injectable } from "tsyringe";
 
 interface IRequest {
@@ -10,12 +11,16 @@ interface IRequest {
 export class UploadCarImageUseCase {
     constructor(
         @inject("CarsImagesRepository")
-        private carsImages: ICarsImagesRepositories
+        private carsImages: ICarsImagesRepositories,
+        @inject("StorageProvider")
+        private storageProvider: IStorageProvider
+
     ) { }
 
     async execute({ car_id, images_name }: IRequest) {
         images_name.forEach(async (image) => {
             await this.carsImages.create(car_id, image)
+            await this.storageProvider.save(image, "cars")
         })
     }
 }
