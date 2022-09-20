3fbaece28739ccc297333fc12af29005ff113b99
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-09-20 13:09:31 -0300

    feat: Implemented Profile user route

--- src/shared/infra/http/routes/user.routes.ts
+++ src/shared/infra/http/routes/user.routes.ts
@@ -5,13 +5,16 @@ import uploadConfig from "@configs/uploads/upload"
 import { CreateUserController } from "../../../../modules/accounts/useCases/createUser/CreateUserController"
 import { UpdateUserAvatarController } from "../../../../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated"
+import { ProfileUserController } from "@modules/accounts/useCases/profileUser/ProfileUserController";
 
 const createUserController = new CreateUserController();
 const updateAvatarUserController = new UpdateUserAvatarController();
+const profileUserController = new ProfileUserController();
 
 const uploadAvatar = multer(uploadConfig)
 
 userRouter.post("/", createUserController.handle)
 userRouter.patch("/avatar", ensureAuthenticated, uploadAvatar.single("avatar"), updateAvatarUserController.handle)
+userRouter.get("/", ensureAuthenticated, profileUserController.handle)
 
 export { userRouter }
