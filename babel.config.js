76c616e16e2b709393625939ffe8d0231d77aa09
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-09-25 12:46:51 -0300

    chore: Babel config

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    presets: [
+        ["@babel/preset-env", { targets: { node: "current" } }],
+        "@babel/preset-typescript",
+    ],
+    plugins: [
+        [
+            "module-resolver",
+            {
+                alias: {
+                    "@errors": ["./src/errors"],
+                    "@configs": ["./src/configs"],
+                    "@modules": ["./src/modules"],
+                    "@shared": ["./src/shared"],
+                    "@utils": ["./src/utils"]
+                },
+            },
+        ],
+        "babel-plugin-transform-typescript-metadata",
+        ["@babel/plugin-proposal-decorators", { legacy: true }],
+        ["@babel/plugin-proposal-class-properties", { loose: true }]
+    ]
+}
