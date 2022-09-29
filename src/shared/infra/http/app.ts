4ff6f1434fa752443e7d5a3237cfb961ee149906
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-09-29 08:32:30 -0300

    feat: Implemented Sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -8,14 +8,33 @@ import { AppError } from "../../errors/AppError";
 import upload from "@configs/uploads/upload";
 import cors from "cors"
 import { rateLimiterMiddleware } from "./middlewares/rateLimiter";
+import * as Sentry from "@sentry/node"
+import * as Tracing from "@sentry/tracing"
+import configs from "@configs/dotenvEntries"
 
 const app = express();
 app.use(rateLimiterMiddleware);
+
+Sentry.init({
+    dsn: configs.sentry.dsn,
+    integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+        new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
+
 app.use(express.json());
 app.use(cors())
 app.use(router);
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/avatar", express.static(`${upload.tmpFolder}/cars`));
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof AppError) {
